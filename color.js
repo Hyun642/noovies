5746a2b7c9c2ec10dd4ae42b47e888d8948a5ed9
Hyun <jjjtoy@naver.com>
2024-05-17 17:43:58 +0900

    다크모드 연습

---
+++ color.js
@@ -0,0 +1,2 @@
+export const YELLOW_COLOR = "#ffa801"
+export const LIGTH_GREY = "#808e9b"
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,43 +3,30 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movie";
 import Search from "../screens/Search";
 import Tv from "../screens/Tv";
-import { View, Text, TouchableOpacity } from "react-native";
+import { useColorScheme } from "react-native";
+import { LIGTH_GREY, YELLOW_COLOR } from "../color";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => 
-    <Tab.Navigator 
-        screenOptions={{   // 모든 스크린에 적용 옵션
-            tabBarLabelPosition:"beside-icon", // 레이블 위치 변경
-            tabBarActiveTintColor: "red", // 선택된 레이블 색깔
-            tabBarInactiveTintColor: "blue", // 선택안된 레이블 색깔
-            tabBarStyle: {
-                backgroundColor: "yellow"
-            },
-            tabBarLabelStyle: { // 레이블 스타일
-                backgroundColor: "gray"
-            },
-
-            headerRight: () => (
-                <TouchableOpacity style={{
-                    backgroundColor: "gray"
-                    }}>
-                        <Text>Hi</Text>
-                </TouchableOpacity>)  
-        }}
-    >
-        <Tab.Screen name="Movies" component={Movies} 
-            options={{  // 해당 스크린 적용 옵션
-            tabBarBadge: 5, // 탭바 알림 배지 메시지 (숫자, 문자)
-            tabBarLabelStyle: { // 해당 레이블 스타일
-                backgroundColor: "pink"
-            }
-                }}/>
+const Tabs = () => {
+    const isDark = useColorScheme() === "dark"
+    return(
+        <Tab.Navigator screenOptions={{tabBarStyle: {
+            backgroundColor: isDark ? "black" : "white"
+        },
+        tabBarActiveTintColor: isDark ? YELLOW_COLOR : "black",
+        tabBarInactiveTintColor: isDark ? "white" : LIGTH_GREY,
+        headerStyle: {
+            backgroundColor: isDark ? "black" : "white"
+        },
+        headerTitleStyle: {color: isDark ? "white" : "black",}
+        }}>
+        <Tab.Screen name="Movies" component={Movies}/>
         <Tab.Screen name="Tv" component={Tv}/>
-        <Tab.Screen name="Search" component={Search} options={{ // 해당 스크린 적용 옵션
-                title:"검색", // 헤더 타이틀
-                headerTitleStyle: { color: "red"} //헤더 스타일
-        }}/>
+        <Tab.Screen name="Search" component={Search}/>
     </Tab.Navigator>
-
+    )
+    
+}
+    
 export default Tabs;
